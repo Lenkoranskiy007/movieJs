73beda241aed504bcb72da5986b436c009125ae8
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-11-06 18:55:18 +0300

    pag

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 
-const API_KEY = '408e3df2-1ec5-416a-9c91-b340bd84c2ba';
-const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1';
+const API_KEY = '408e3df2-1ec5-416a-9c91-b340bd84c2ba'
+const API_URL_TOP = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1'
 const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
-getMovieFunc(API_URL_POPULAR)
 
+getMovies(API_URL_TOP)
 
-
-async function getMovieFunc(url) {
-  const response = await axios.get(url, {
+ 
+async function getMovies (url) {
+   
+  const response  = await axios.get(url, {
     headers: {
       'Content-type': 'application/json',
       'X-API-KEY': API_KEY
     }
-    })
-    movieData(response.data)
-    console.log(response.data)
+  }).then( res => res.data)
+
+  console.log(response)
+  setMovies(response)
+
 }
 
-
-const movieData = (data) => {
+function setMovies(movieData) {
   const movies = document.querySelector('.movies')
   movies.innerHTML = ''
-    data.films.map(item => {
+  movieData.films.map(item => {
     const movie = document.createElement('div')
     movie.classList.add('movie')
-     movie.innerHTML = `
+    movie.innerHTML = `
     <div class="movie__cover__inner">
     <img
       src="${item.posterUrlPreview}"
@@ -35,45 +37,40 @@ const movieData = (data) => {
   </div>
   <div class="movie__info">
     <div class="movie__info__title">${item.nameRu}</div>
-    <div class="movie__info__category">${item.genres.map(genres =>  genres.genre + ' ')}</div>
-
-    ${item.rating && `<div class="movie__info__average movie__info__average__${getByClass(item.rating)}"> ${item.rating} </div> 
-    `}
-
+    <div class="movie__info__category">${item.genres.map(genres => genres.genre)}</div>
+    ${item.rating && `<div class="movie__info__average movie__info__average__${getByClass(item.rating)}">${item.rating}</div> `}
     <div class="movie__info__year">${item.year}</div>
-  </div>
-    
+
+    </div>
     `
     movies.appendChild(movie)
     return item
-    
-
   })
 }
 
-function getByClass (rating) {
+function getByClass(rating) {
   if (rating >= 7) {
     return 'green'
-  } else if (rating >= 5) {
-     return 'orange'
-  } else  {
+  } else if (rating >= 5)  {
+    return 'orange'
+  } else {
     return 'red'
   }
 }
 
 
 const form = document.querySelector('form')
-const inputSearch = document.querySelector('.header__content__search')
+const input  = document.querySelector('.header__content__search')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-
-  const apiSearchUrl = `${API_URL_SEARCH}${inputSearch.value}`
-  if(inputSearch.value) {
-    getMovieFunc(apiSearchUrl)
+  const searchData = `${API_URL_SEARCH}${input.value}`
+  if (input.value)  {
+    getMovies(searchData)
   }
 
- })
+
+})
 
 
 
