67ec4bf9240ae2eb725b52a384f2b62d67d662a1
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2021-11-07 11:39:22 +0300

    edit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/searc
 
 getMovies(API_URL_TOP)
 
- 
 async function getMovies (url) {
    
   const response  = await axios.get(url, {
@@ -26,13 +25,14 @@ function setMovies(movieData) {
     const movie = document.createElement('div')
     movie.classList.add('movie')
     movie.innerHTML = `
-    <div class="movie__cover__inner">
+    <div class="movie__cover__inner" >
     <img
-      src="${item.posterUrlPreview}"
-      alt="${item.posterUrlPreview}"
-      class="movie__cover"
-    />
-    <div class="movie__cover__darkened"></div>
+    src="${item.posterUrlPreview}"
+    alt="${item.posterUrlPreview}"
+    class="movie__cover"
+  />
+   
+    <div class="movie__cover__darkened" data-movie="movie"></div>
   </div>
   <div class="movie__info">
     <div class="movie__info__title">${item.nameRu}</div>
@@ -59,7 +59,8 @@ function getByClass(rating) {
 
 
 const form = document.querySelector('form')
-const input  = document.querySelector('.header__content__search')
+const input  = document.querySelector('.form__input')
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -68,12 +69,9 @@ form.addEventListener('submit', (e) => {
     getMovies(searchData)
   }
 
-
 })
 
 const items = document.querySelectorAll('.pagination li')
-let notesOnPage = 8;
-
  for (let item of items) {
     item.addEventListener('click', function () {
       let pageNum = +this.innerHTML     
@@ -82,8 +80,126 @@ let notesOnPage = 8;
     })
  }
 
- 
-   
+ const logo = document.getElementById('logo')
+ logo.addEventListener('click', event => {
+   const url = `https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1`
+   getMovies(url)
+ })
+
+
+// const openPopup = document.querySelector('.popup__open')
+// const closePopup = document.querySelector('.popup__close')
+// const popup = document.querySelector('.popup')
+// const popupBg = document.querySelector('.popup__container')
+
+
+
+// function openModal (movieData) {
+//   const popup = document.querySelector('.popup')
+// }
+
+
+// openPopup.addEventListener('click', (event) => {
+//   event.preventDefault()
+//   popup.classList.add('active')
+// })
+
+// closePopup.addEventListener('click', (event) => {
+//   popup.classList.remove('active')
+// })
+
+
+// document.addEventListener('click', event => {
+//   if (event.target === popupBg ) {
+//     popup.classList.remove('active')
+//   } 
+// })
+
+// document.addEventListener('click', event => {
+//   if (event.target.dataset.movie) {
+//     popup.classList.add('active')
+//   } 
+// })
+
+
+
+function _createModal (options) {
+  const popup = document.createElement('div')
+  popup.classList.add('popup')
+  popup.insertAdjacentHTML('afterbegin', `
+    <div class="popup__container" data-popup="popup"\>
+          <div class="popup__body">
+            <img src="" alt="" />
+            <div class="movie__info">
+              <div class="movie__info__title">ddkkdkdkdkkdkddkkdkd</div>
+              <div class="movie__info__category">dkdkdkdkkdkdkdkdk</div>
+              <div class="movie__info__average">ddkdkdkdkdkkdkd</div>
+              <div class="movie__info__year">ddkdkdkdkkdkdkdk</div>
+            </div>
+            <div class="popup__close" data-close="close">&#10006</div>
+          </div>
+        </div>
+
+  `)
+
+  document.body.appendChild(popup)
+  return popup
+
+}
+
+
+
+
+$.modal = function (options) {
+
+  const $modal = _createModal(options)
+  return {
+    open() {
+      $modal.classList.add('active')
+    },
+
+    close () {
+      console.log('close')
+      $modal.classList.remove('active')
+
+    }
+  }
+}
+
+document.addEventListener('click', event => {
+  event.preventDefault()
+  if (event.target.dataset.movie) {
+    modal.open()
+  }
+})
+
+document.addEventListener('click', event => {
+  if (event.target.dataset.close ) {
+    modal.close()
+  }
+})
+
+document.addEventListener('click', event => {
+  if (event.target.dataset.close ) {
+    modal.close()
+  }
+})
+
+document.addEventListener('click', event => {
+  if (event.target.dataset.popup ) {
+    modal.close()
+  }
+})
+
+
+
+
+const modal = $.modal()
+
+
+
+
+
 
 
 
